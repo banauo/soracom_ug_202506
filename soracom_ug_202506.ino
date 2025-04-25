0d7870c2adc1188fc290cec4ea23f4f86012e2ad
CHIYA yuji <banauo@gmail.com>
2025-04-25 23:47:34 +0900

    add: DHT22, measure temp&hum

--- soracom_ug_202506.ino
+++ soracom_ug_202506.ino
@@ -15,6 +15,7 @@
 #include <WioCellular.h>
 #include <ArduinoJson.h>
 #include <ArduinoHttpClient.h>
+#include <DHT.h>  // Include DHT library
 
 #define SEARCH_ACCESS_TECHNOLOGY (WioCellularNetwork::SearchAccessTechnology::LTEM)  // https://seeedjp.github.io/Wiki/Wio_BG770A/kb/kb4.html
 #define LTEM_BAND (WioCellularNetwork::NTTDOCOMO_LTEM_BAND)                          // https://seeedjp.github.io/Wiki/Wio_BG770A/kb/kb4.html
@@ -33,6 +34,11 @@ static constexpr int POWER_ON_TIMEOUT = 1000 * 20;     // [ms]
 static constexpr int NETWORK_TIMEOUT = 1000 * 60 * 2;  // [ms]
 static constexpr int RECEIVE_TIMEOUT = 1000 * 10;      // [ms]
 
+#define DHTPIN D28  // Pin for DHT22 sensor
+#define DHTTYPE DHT22  // Define sensor type as DHT22
+
+DHT dht(DHTPIN, DHTTYPE);  // Create DHT object
+
 struct HttpResponse {
   int statusCode;
   std::map<std::string, std::string> headers;
@@ -42,8 +48,8 @@ struct HttpResponse {
 struct SensorData {
   int distance;
   int co2;
-  int temperature;
-  int humidity;
+  float temperature;
+  float humidity;
   const char* measure_date;
 };
 
@@ -81,6 +87,9 @@ void setup(void) {
   // Start WioCellular
   WioCellular.begin();
 
+  // Initialize DHT sensor
+  dht.begin();
+
   // Power on the cellular module
   if (WioCellular.powerOn(POWER_ON_TIMEOUT) != WioCellularResult::Ok) abort();
   WioNetwork.begin();
@@ -93,6 +102,10 @@ void setup(void) {
 
 void loop(void) {
   digitalWrite(LED_BUILTIN, HIGH);
+  WioCellular.enableGrovePower();
+
+  // Wait for DHT22 sensor to stabilize
+  delay(1000);
 
   HttpResponse response;
   {
@@ -111,12 +124,24 @@ void loop(void) {
     Serial.println("Response body does not contain a valid number.");
   }
 
-  // Create SensorData instance and populate it with sample data
+  // Create SensorData instance and populate it with sensor data
   SensorData sensorData;
   sensorData.distance = 100;  // Example value
   sensorData.co2 = 400;       // Example value
-  sensorData.temperature = 25; // Example value
-  sensorData.humidity = 60;    // Example value
+
+  // Read temperature and humidity from DHT22 sensor
+  sensorData.temperature = dht.readTemperature();  // Get temperature
+  sensorData.humidity = dht.readHumidity();        // Get humidity
+
+  // Check for sensor read errors
+  if (isnan(sensorData.temperature) || isnan(sensorData.humidity)) {
+    Serial.println("Failed to read from DHT sensor!");
+    sensorData.temperature = 0;  // Default value
+    sensorData.humidity = 0;     // Default value
+  }
+
+  WioCellular.disableGrovePower();
+
   sensorData.measure_date = "2025-04-25T12:00:00Z"; // Example ISO 8601 date
 
   JsonDoc.clear();
